9af73249f8b849ec2bc61b799383a081710276f5
RichaBharti0603 <richab820@gmail.com>
2025-04-05 19:43:16 +0530

    Final commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-export default defineConfig(({ mode }) => ({
-  base: "/DisasterManagement/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  base: '/DisasterManagement/',
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    target: "esnext",
-  },
-}));
+  server: {
+    host: true,
+  }
+})
